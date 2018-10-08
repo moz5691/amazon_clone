30e902194239d0548972825c99a88e088a8c28f6
sandy8111112004 <mingshiuantsai1992@gmail.com>
2018-10-07 22:14:22 -0400

    set <base href="/">

--- public/javascripts/purchase.js
+++ public/javascripts/purchase.js
@@ -13,31 +13,32 @@ const incFunc = function(e){
 }
 
 
-const purchaseFunc = function(e){
-    console.log('get in purchase');
-    e.preventDefault();
-    const id = $(this).attr('data-id');
-    console.log(id);
-    $.ajax({url:`/purchase/:${id}`, method: 'GET', data: {id:id}})
-    .then(
-        function(){
-            console.log('get into purchase page');
-        }
-    ).catch(
-        function(){
-            alert("There's a problem happened in purchaseFunc!");
-        }
-    );
+// const purchaseFunc = function(e){
+//     console.log('get in purchase');
+//     e.preventDefault();
+//     const id = $(this).attr('data-id');
+//     console.log(id);
+//     $.ajax({url:`/purchase/:${id}`, method: 'GET', data: {id:id}})
+//     .then(
+//         function(){
+//             console.log('get into purchase page');
+//         }
+//     ).catch(
+//         function(){
+//             alert("There's a problem happened in purchaseFunc!");
+//         }
+//     );
     
-}
+// }
 
 console.log('in pruchase.js');
 $(document).on('click','#incQtyBtn',incFunc);
 //$(document).on('click','#buyBtn',purchaseFunc);
 
-
+/*
 const cartFunc = function(e){
     e.preventDefault();
 }
 
 $(document).on('click','#cartBtn',cartFunc)
+*/
