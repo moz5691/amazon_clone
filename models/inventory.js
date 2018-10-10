e80dd770ed876d07b099bb69f050da53206b2659
k_maryam27 <k_maryam27@yahoo.com>
2018-10-09 20:35:32 -0400

    remove seller from inventory

--- models/inventory.js
+++ models/inventory.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const InventorySchema = new Schema({
-  seller: {
-    type: String,
-    required: true
-  },
   itemName: {
     type: String,
     required: true
