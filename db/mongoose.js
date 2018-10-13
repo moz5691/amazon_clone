f00879f0597385f2fef40a7629357ea7991de5bd
k_maryam27 <k_maryam27@yahoo.com>
2018-10-13 13:16:02 -0400

    remove all validators of users

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,8 +3,8 @@ mongoose.Promise = global.Promise;
 
 mongoose
   .connect(
-      'mongodb://amazon:amazon123@ds121183.mlab.com:21183/amazon'
-    // 'mongodb://localhost/amazon'
+      // 'mongodb://amazon:amazon123@ds121183.mlab.com:21183/amazon'
+    'mongodb://localhost/amazon'
   )
   .then(() => console.log('Mongodb connected...'))
   .catch(err => console.log(err));
