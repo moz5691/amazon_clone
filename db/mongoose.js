f13dc56a2958f98df2542ecf8c356706d3440e36
tnguyen303 <tringuyen552911@gmail.com>
2018-10-13 14:00:12 -0400

    no changes

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,9 +3,10 @@ mongoose.Promise = global.Promise;
 
 mongoose
   .connect(
-      'mongodb://amazon:amazon123@ds121183.mlab.com:21183/amazon',
-      {useNewUrlParser: true}
-    // 'mongodb://localhost/amazon'
+    // 'mongodb://amazon-2:amazon-2@ds131763.mlab.com:31763/amazon-2',
+    'mongodb://amazon:amazon123@ds121183.mlab.com:21183/amazon',
+    // 'mongodb://localhost/amazon',
+    { useNewUrlParser: true }
   )
   .then(() => console.log('Mongodb connected...'))
   .catch(err => console.log(err));
