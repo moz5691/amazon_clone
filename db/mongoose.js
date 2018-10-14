793854df164bba9d1ba6cfd13add34b6cee39338
sandy8111112004 <mingshiuantsai1992@gmail.com>
2018-10-13 20:19:28 -0400

    test commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,9 +3,9 @@ mongoose.Promise = global.Promise;
 
 mongoose
   .connect(
-      'mongodb://amazon:amazon123@ds121183.mlab.com:21183/amazon',
-      {useNewUrlParser: true}
-    // 'mongodb://localhost/amazon'
+     // 'mongodb://amazon:amazon123@ds121183.mlab.com:21183/amazon',
+     // {useNewUrlParser: true}
+     'mongodb://localhost/amazon1'
   )
   .then(() => console.log('Mongodb connected...'))
   .catch(err => console.log(err));
--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -1,4 +1,4 @@
-const Inventory = require('./../models/inventory');
+// const Inventory = require('./../models/inventory');
 
 // Inventory.find({ itemInCart: true }).then(
 //     function (data) {
@@ -13,6 +13,67 @@ const Inventory = require('./../models/inventory');
 // ).catch(
 //     console.log('error heppens in cart script')
 // );
+
+const cartIncFunc = function(e){
+    console.log('in cartIncFunc');
+    e.preventDefault();
+    const productID=$(this).attr('data-id');
+
+    const incStep = parseFloat($('#cartSelect').val());
+    let tempCart = parseFloat(sessionStorage.getItem(`${productID}`)) || 0;
+    tempCart += incStep;
+    sessionStorage.setItem(`${productID}`,tempCart);
+    $('#cartQty').text(`${sessionStorage.getItem(`${productID}`)}`);
+    console.log($('#cartQty').text());
+    let inCart = false;
+    if (sessionStorage.getItem(`${productID}`) !=='0'){
+        inCart=true;
+    }
+    const updateEntry={
+    itemInCart:inCart
+    }
+    $.ajax({url:`/cartUpdate/${productID}`,method:'PUT',data:updateEntry }).then(
+        function(data){
+            if(data){
+                console.log('itemInCart successfully change');
+            }else{
+                console.log("There's some problem in put methord ")
+            }
+        }
+    );
+}
+$(document).on('click','#cartIncBtn',cartIncFunc);
+
+
+const cartDecFunc = function(e){
+    console.log('in cartDecFunc');
+    e.preventDefault();
+    const productID=$(this).attr('data-id');
+
+    const decStep = parseFloat($('#cartSelect').val());
+    let tempCart = parseFloat(sessionStorage.getItem(`${productID}`)) || 0;
+    tempCart -= DecStep;
+    sessionStorage.setItem(`${productID}`,tempCart);
+    $('#cartQty').text(`${sessionStorage.getItem(`${productID}`)}`);
+    console.log($('#cartQty').text());
+    let inCart = false;
+    if (sessionStorage.getItem(`${productID}`) !=='0'){
+        inCart=true;
+    }
+    const updateEntry={
+    itemInCart:inCart
+    }
+    $.ajax({url:`/cartUpdate/${productID}`,method:'PUT',data:updateEntry }).then(
+        function(data){
+            if(data){
+                console.log('itemInCart successfully change');
+            }else{
+                console.log("There's some problem in put methord ")
+            }
+        }
+    );
+}
+$(document).on('click','#cartDecBtn',cartDecFunc);
 
 
 
