a2604e3825107930fc5fc1399d9a5671af37d3f3
sandy8111112004 <mingshiuantsai1992@gmail.com>
2018-10-20 10:02:10 -0400

    change database connecting

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,9 +4,9 @@ mongoose.Promise = global.Promise;
 mongoose
   .connect(
     //'mongodb://amazon-2:amazon-2@ds131763.mlab.com:31763/amazon-2',
-    //  'mongodb://amazon:amazon123@ds121183.mlab.com:21183/amazon',
-    // {useNewUrlParser: true}
-      'mongodb://localhost/amazon1'
+      'mongodb://amazon:amazon123@ds121183.mlab.com:21183/amazon',
+     {useNewUrlParser: true}
+    //  'mongodb://localhost/amazon1'
   )
   .then(() => console.log('Mongodb connected...'))
   .catch(err => console.log(err));
